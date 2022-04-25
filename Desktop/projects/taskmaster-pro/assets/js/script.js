118711b74d73dd2772727b13009dad34f947bc07
Slimeball <alexramo2330@gmail.com>
2022-04-24 20:28:05 -0500

    ui-touchup

--- Desktop/projects/taskmaster-pro/assets/js/script.js
+++ Desktop/projects/taskmaster-pro/assets/js/script.js
@@ -71,6 +71,7 @@ var auditTask = function(taskEl) {
   else if (Math.abs(moment().diff(time, "days")) <= 2) {
     $(taskEl).addClass("list-group-item-warning");
   }
+  console.log(taskEl);
 };
 
 // enable draggable/sortable feature on list-group elements
@@ -81,15 +82,23 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event, ui) {
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
     console.log(ui);
   },
   deactivate: function(event, ui) {
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass(".bottom-trash-drag");
     console.log(ui);
   },
   over: function(event) {
+    $(event.target).addClass("dropover-active");
+    $(".bottom-trash").addClass(".bottom-trash-active");
     console.log(event);
   },
   out: function(event) {
+    $(event.target).removeClass("dropover-active");
+    $(".bottom-trash").removeClass(".bottom-trash-active");
     console.log(event);
   },
   update: function() {
@@ -288,3 +297,9 @@ $("#remove-tasks").on("click", function() {
 
 // load tasks for the first time
 loadTasks();
+
+setInterval(function() {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, 1800000);
